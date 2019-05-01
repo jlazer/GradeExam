05e10f791c608a47702605896bf62b427e076204
Justin Lazarski <blackhawks189@gmail.com>
2019-04-30 21:05:18 -0700

    Initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //TODO:Write a main method that holds the correct answers, asks the user to enter a student's answers, call the above two methods, and then displays the results.
+        // TODO:The program should use a constant for the maximum number of choice questions.
+        
+        System.out.println("Please enter the student numbers.");
+
+    }
+
+    public static int gradeExam(char[] correct, char[] student) {
+        //TODO:This method will determine the number of incorrect answers. Be sure that the comparison is not case sensitive.
+    }
+    public static int[] makeMissedArray(char[] correct, char[] student, int numIncorrect) {
+        //TODO:This method will make the missed array and stores the numbers of all the questions that the student missed in it.
+    }
+
+}
