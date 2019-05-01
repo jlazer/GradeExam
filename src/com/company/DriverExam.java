29cb3fcd2b18716f69eaa03c7bf17fc4fc1203ec
Justin Lazarski <blackhawks189@gmail.com>
2019-04-30 21:42:39 -0700

    Created a loop that will print the question number. Also Created a DriverExam Class that has the correctAnswers array.

---
+++ src/com/company/DriverExam.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class DriverExam {
+    private String[] correctAnswers = {"A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "C", "C", "C", "C", "C", "D", "D", "D", "D", "D"};
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,45 @@
+//Justin Lazarski
+//CIS 3090.04
+//4/30/19
+//Extra Credit Question
+
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         //TODO:Write a main method that holds the correct answers, asks the user to enter a student's answers, call the above two methods, and then displays the results.
         // TODO:The program should use a constant for the maximum number of choice questions.
 
-        System.out.println("Please enter the student numbers.");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Driver's license exam");
+        System.out.println("20 Multiple Choice Questions");
+        System.out.println("Mark A, B, C, D");
+        System.out.println("Please enter your answers to the questions");
+
+        String[] answersArray = new String[20];
+        String answer;
+
+        for(int i = 1; i < 21; i++) {
+            System.out.println("Question " + i + ": ");
+            
+        }
+
+
 
     }
 
     public static int gradeExam(char[] correct, char[] student) {
         //TODO:This method will determine the number of incorrect answers. Be sure that the comparison is not case sensitive.
+        int testInt = 0;
+        return testInt;
     }
     public static int[] makeMissedArray(char[] correct, char[] student, int numIncorrect) {
         //TODO:This method will make the missed array and stores the numbers of all the questions that the student missed in it.
+        int[] testIntArray = {0};
+        return testIntArray;
     }
 
 }
